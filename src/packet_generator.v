84e8f68040d80ddd593eeb155acc810536eb7464
Joel Holdsworth <joel@airwebreathe.org.uk>
2016-06-14 23:07:20 -0600

    package_generator: Clock generation and send triggering

--- src/packet_generator.v
+++ src/packet_generator.v
@@ -1,15 +1,16 @@
-module packet_generator(output out, input clk, input reset);
+module packet_generator(output out, input ref_clk, input reset, input start);
 
 	parameter CTR_WIDTH = 8;
 	parameter ID_WIDTH = 4;
 	parameter CMD_WIDTH = 7;
 	parameter PACKET_OFFSET_WIDTH = 4;
 
-	reg out;
-	wire clk, reset;
+	wire out, clk, reset;
 
 	reg packet_sending;
-	reg data;
+	reg data, pwm;
+
+	reg [12:0] protocol_clk_phase;
 
 	reg [1:0] pwm_phase;
 	reg [PACKET_OFFSET_WIDTH : 0] packet_offset;
@@ -20,41 +21,55 @@ module packet_generator(output out, input clk, input reset);
 	wire [CMD_WIDTH : 0] cmd;
 	assign cmd = 7'b0010111;
 
-	always @(posedge clk)
+	reg out;
+
+	always @(posedge ref_clk)
 	begin
-		case(packet_offset)
-			2: data = id[0];
-			3: data = id[1];
-			4: data = id[2];
-			5: data = id[3];
-			6: data = cmd[0];
-			7: data = cmd[1];
-			8: data = cmd[2];
-			9: data = cmd[3];
-			10: data = cmd[4];
-			11: data = cmd[5];
-			12: data = cmd[6];
-			default: data = 0;
-		endcase
-
-		if (packet_offset == 13)
-			packet_sending <= 0;
-
-		case(pwm_phase)
-			2'b00 : out <= 0;
-			2'b01 : out <= data;
-			2'b10 : out <= packet_sending;
-		endcase
+		if (protocol_clk_phase == 0) begin
+			protocol_clk_phase <= 1835;
+		end else begin
+			protocol_clk_phase <= protocol_clk_phase - 1;
+		end
 
 		if (reset) begin
+			packet_sending <= 0;
+		end else if (start) begin
 			pwm_phase <= 0;
 			packet_offset <= 0;
 			packet_sending <= 1;
-		end else if (pwm_phase == 2'b10) begin
-			pwm_phase <= 2'b00;
-			packet_offset <= packet_offset + 1;
-		end else
-			pwm_phase <= pwm_phase + 1;
+			protocol_clk_phase <= 0;
+		end else if (protocol_clk_phase == 0) begin
+			if (packet_offset == 13)
+				out <= 0;
+			else begin
+				if (pwm_phase == 2'b10) begin
+					pwm_phase <= 2'b00;
+					packet_offset <= packet_offset + 1;
+				end else
+					pwm_phase <= pwm_phase + 1;
+
+				case(packet_offset)
+					2: data <= id[0];
+					3: data <= id[1];
+					4: data <= id[2];
+					5: data <= id[3];
+					6: data <= cmd[0];
+					7: data <= cmd[1];
+					8: data <= cmd[2];
+					9: data <= cmd[3];
+					10: data <= cmd[4];
+					11: data <= cmd[5];
+					12: data <= cmd[6];
+					default: data <= 0;
+				endcase
+
+				case(pwm_phase)
+					2'b00 : out <= 0;
+					2'b01 : out <= data;
+					2'b10 : out <= 1;
+				endcase
+			end
+		end
 	end
 
 endmodule // counter
