ebf13c3a9b00fa828d4f629ac7107a4bf9766a10
Joel Holdsworth <joel@airwebreathe.org.uk>
2016-06-23 10:29:43 -0600

    packet_generator: Made output more structural

--- src/packet_generator.v
+++ src/packet_generator.v
@@ -6,7 +6,7 @@ module packet_generator(output out, input ref_clk, input reset, input [2:0] cmd,
 	parameter CMD_WIDTH = 7;
 	parameter PACKET_OFFSET_WIDTH = 4;
 
-	wire out, clk, reset;
+	wire clk, reset;
 
 	reg packet_sending;
 	reg data, pwm;
@@ -25,12 +25,27 @@ module packet_generator(output out, input ref_clk, input reset, input [2:0] cmd,
 
 	always @(*) begin
 		case (cmd)
-			0: payload <= 7'b1001111;
-			1: payload <= 7'b1000111;
-			2: payload <= 7'b0100111;
-			3: payload <= 7'b0010111;
-			4: payload <= 7'b0001111;
-			default: payload <= 7'b0000000;
+			0: payload = 7'b1001111;
+			1: payload = 7'b1000111;
+			2: payload = 7'b0100111;
+			3: payload = 7'b0010111;
+			4: payload = 7'b0001111;
+			default: payload = 7'b0000000;
+		endcase
+
+		case(packet_offset)
+			2: data = id[0];
+			3: data = id[1];
+			4: data = id[2];
+			5: data = id[3];
+			6: data = payload[0];
+			7: data = payload[1];
+			8: data = payload[2];
+			9: data = payload[3];
+			10: data = payload[4];
+			11: data = payload[5];
+			12: data = payload[6];
+			default: data = 0;
 		endcase
 	end
 
@@ -58,26 +73,10 @@ module packet_generator(output out, input ref_clk, input reset, input [2:0] cmd,
 					packet_offset <= packet_offset + 1;
 				end else
 					pwm_phase <= pwm_phase + 1;
-
-				case(packet_offset)
-					2: data <= id[0];
-					3: data <= id[1];
-					4: data <= id[2];
-					5: data <= id[3];
-					6: data <= payload[0];
-					7: data <= payload[1];
-					8: data <= payload[2];
-					9: data <= payload[3];
-					10: data <= payload[4];
-					11: data <= payload[5];
-					12: data <= payload[6];
-					default: data <= 0;
-				endcase
-
 				case(pwm_phase)
-					2'b00 : out <= 0;
-					2'b01 : out <= data;
-					2'b10 : out <= 1;
+					2'b00: out <= 0;
+					2'b01: out <= data;
+					default: out <= 1;
 				endcase
 			end
 		end
