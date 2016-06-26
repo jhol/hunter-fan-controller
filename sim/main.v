b199dffe9be0ff98416c6c87022fc8e07d753d58
Joel Holdsworth <joel@airwebreathe.org.uk>
2016-06-25 23:44:18 -0600

    Fixed simulation

--- sim/main.v
+++ sim/main.v
@@ -1,17 +1,22 @@
 module test;
 	reg reset = 0;
+	reg start = 0;
 	initial begin
-		$dumpfile("protocol.vcd");
+		$dumpfile("packet_generator.vcd");
 		$dumpvars(0, test);
 
 		# 3 reset = 1;
-		# 1 reset = 0;
-		# 513 $finish;
+		# 2 reset = 0;
+		# 10 start = 1;
+		# 2 start = 0;
+		# 500000 $finish;
 	end
 
 	reg clk = 0;
 	always #1 clk = !clk;
 
-	wire value;
-	packet_generator g1 (value, clk, reset);
+	reg [2:0] cmd = 0;
+
+	wire value, sending;
+	packet_generator g1 (value, sending, clk, reset, cmd, start);
 endmodule
